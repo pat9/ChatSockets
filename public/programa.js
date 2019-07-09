473e5957a3e0078165dc282386e2738eee59202b
Daniel Pat <danielpat6@gmail.com>
2019-07-09 09:04:05 -0500

    audio

--- public/programa.js
+++ public/programa.js
@@ -16,24 +16,36 @@ p2p.on('stream', function (stream) {
 })
 
 function startStream () {
-  startButton.setAttribute('disabled', true)
-  navigator.getUserMedia({ audio: true }, function (stream) {
-    var audioContext = new window.AudioContext()
-    var mediaStreamSource = audioContext.createMediaStreamSource(stream)
-    var mediaStreamDestination = audioContext.createMediaStreamDestination()
-    mediaStreamSource.connect(mediaStreamDestination)
-
-    var socket = io()
-    var p2p = new P2P(socket, {peerOpts: {stream: mediaStreamDestination.stream}})
-
-    p2p.on('ready', function () {
-      p2p.usePeerConnection = true
-    })
-
-    p2p.emit('ready', { peerId: p2p.peerId })
-  }, function (err) {
-    console.log(err)
-  })
+  navigator.permissions.query({name:'microphone'}).then(function(result) {
+    if (result.state == 'granted') {
+  
+    } else if (result.state == 'prompt') {
+  
+    } else if (result.state == 'denied') {
+  
+    }
+    result.onchange = function() {
+      startButton.setAttribute('disabled', true)
+      navigator.getUserMedia({ audio: true }, function (stream) {
+        var audioContext = new window.AudioContext()
+        var mediaStreamSource = audioContext.createMediaStreamSource(stream)
+        var mediaStreamDestination = audioContext.createMediaStreamDestination()
+        mediaStreamSource.connect(mediaStreamDestination)
+    
+        var socket = io()
+        var p2p = new P2P(socket, {peerOpts: {stream: mediaStreamDestination.stream}})
+    
+        p2p.on('ready', function () {
+          p2p.usePeerConnection = true
+        })
+    
+        p2p.emit('ready', { peerId: p2p.peerId })
+      }, function (err) {
+        console.log(err)
+      })
+    };
+  });
+ 
 }
 
 startButton.addEventListener('click', startStream)
